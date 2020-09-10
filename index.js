3843d4e1b340679530c53a9da5302cbe01f3b32d
kai-blt <blaitong@gmail.com>
2020-09-10 07:42:08 -0700

    completed task 6

--- index.js
+++ index.js
@@ -95,6 +95,9 @@ reviews.forEach(item => item.name === 'Reyna' ? item.feedback = 'this place is c
 console.table(reviews);
 
 
+
+
+
 /*  Task 6: Write a function to return a review based on the index of the review in the array.
 
  Your function should take two arguments:
@@ -108,9 +111,13 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
 function getReviewByIndex(reviews, index) {
-    /* code here */
-  }
-  
+    return `${reviews[index].name} gave the restaurant a ${reviews[index].rating}, and their feedback was: ${reviews[index].feedback}`;
+}
+console.log(getReviewByIndex(reviews, 0));  
+
+
+
+
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
 
